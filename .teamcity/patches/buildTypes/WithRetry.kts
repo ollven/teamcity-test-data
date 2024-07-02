7d75e185dc67cbd0888589136bd942234126a233
ollven <ollven@TeamCity>
2024-07-02 15:18:15 +0200

    TeamCity change in 'Add mute with smaller scope / Flaky Tests' project: runners of 'WithRetry' build configuration were updated

--- .teamcity/patches/buildTypes/WithRetry.kts
+++ .teamcity/patches/buildTypes/WithRetry.kts
@@ -27,7 +27,7 @@ create(DslContext.projectId, BuildType({
     steps {
         script {
             scriptContent = """
-                echo "##teamcity[testRetrySupport enabled 'true']"
+                echo "##teamcity[testRetrySupport enabled="true"]"
                 ./generate_fake_run.sh %system.ALL_TESTS_NUM% %system.FAILED_TESTS_PERCENTAGE% %system.TEST_LOG_LINES_NUM%
             """.trimIndent()
         }
